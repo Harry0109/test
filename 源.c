660ebf6d15f2aaa5354dc66d8acfc1bfb3f4bc27
Harry0109 <101096891+Harry0109@users.noreply.github.com>
2022-03-07 17:52:52 +0800

    Add files via upload

---
+++ 源.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+int main() {
+	//int x, y, z,max;
+	//scanf("%d%d%d", &x, &y, &z);
+	//max = x;
+	//if (x < y) {
+	//	max = y;
+	//	y = x;
+	//	x = max;
+	//}
+	//if (x < z) {
+	//	max = z;
+	//}
+	//printf("%d", max);
+
+	//double x;
+	//int y;
+	//while(1){
+	//printf("请输入一个数字\n");
+	//scanf("%lf", &x);
+	//if (x >0 && x < 1000) {
+	//	y = sqrt(x);
+	//	printf("平方根 % d", y);
+	//	break;
+	//}
+	//else
+	//{
+	//	printf("输入错误，重新输入\n");
+	//	continue;
+	//}
+
+	}
+}
